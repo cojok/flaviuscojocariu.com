52628f84fb72ddea665b4e55e7edcf8c7c3becc7
Flavius Cojocariu <cojokka@gmail.com>
2023-10-31 10:09:07 +0100

    gitlab ci yam changes to deploy gitlab pages astro config for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://flaviuscojocariu.com',
+  outDir: 'public',
+  publicDir: 'static',
   integrations: [sitemap()]
 });
